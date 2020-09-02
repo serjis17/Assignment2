253c896cc2e1acf10ec47e4d7ef33e219be448e7
serjis17 <serjis17@gmail.com>
2020-09-02 15:58:39 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,18 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+ constructor(private fb:FormBuilder)
+ {
+
+ }
+ form = this.fb.group({email:['',[Validators.required,Validators.pattern( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+ password:['',[Validators.required,Validators.pattern(
+   /^(?=.*[a-zA-Z0-9])(?=.*[@#$%^&*()]).{8,}$/
+ )]],
+ confirmpassword:['',Validators.required],
+ gender:['',Validators.required],
+ accept:['',Validators.required]
+
+
+ })
 }
