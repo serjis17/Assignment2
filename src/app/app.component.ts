799606f021376619319ea1ba190c20be5f705232
serjis17 <serjis17@gmail.com>
2020-09-03 13:02:11 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup,
+FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,18 +8,39 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
- constructor(private fb:FormBuilder)
+  form:FormGroup
+   constructor(private fb:FormBuilder)
  {
+   
+
 
  }
- form = this.fb.group({email:['',[Validators.required,Validators.pattern( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+ ngOnInit()
+ {
+   this.form = this.fb.group({email:['',[Validators.required,Validators.pattern( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
  password:['',[Validators.required,Validators.pattern(
    /^(?=.*[a-zA-Z0-9])(?=.*[@#$%^&*()]).{8,}$/
  )]],
- confirmpassword:['',Validators.required],
+ confirmpassword:['',[Validators.required,this.passwordMatcher.bind(this)]],
  gender:['',Validators.required],
  accept:['',Validators.required]
 
 
- })
+ });
+ }
+
+ //form:FormBuilder;
+
+
+ 
+// confirm new password validator
+ passwordMatcher(control: FormControl): { [s: string]: boolean } {
+    if (
+       
+        (control.value !== this.form.controls.value)
+    ) {
+        return { passwordNotMatch: true };
+    }
+    return null;
+}
 }
